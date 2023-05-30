f024f55d9f4c0350841a4c4bdd6fd0c604730b40
Alexandre <alx.delira@gmail.com>
2023-05-30 10:25:55 -0400

    Ajustes de rota controler

--- src/controller/rotaController.js
+++ src/controller/rotaController.js
@@ -1,143 +1,92 @@
-import Usuario from "../models/usuario.js";
+import Rotas from "../models/rota.js";
 
-export default class UsuarioController {
-  static cadastrarUsuario = async (req, res) => {
+class RotaController {
+  static listarRotas = async (req, res) => {
     try {
-      const { nome, email, senha, ativo } = req.body;
-
-      if (senha.length < 8) {
-        return res.status(400).json({ error: true, code: 400, message: "Necessario minimo de 8 caracteres" })
-      }
-      
-
-      const novoUsuario = new Usuario({
-        nome,
-        email,
-        senha,
-        ativo
-      });
-
-
-      const usuarioSalvo = await novoUsuario.save()
-
-      return res.status(201).json(usuarioSalvo);
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
-    }
-  }
-
-  static listarUsuarios = async (req, res) => {
-    try {
-      const { nome, email, ativo } = req.query;
+      const { rota } = req.query;
       const { page, perPage } = req.query;
       const options = {
         page: parseInt(page) || 1,
         limit: parseInt(perPage) > 10 ? 10 : parseInt(perPage) || 10
       }
-      if (nome) {
-        const usuarios = await Usuario.paginate({ nome: RegExp(nome, "i") }, options);
-        return res.status(200).json(usuarios);
+      if (rota) {
+        const rotas = await Rotas.paginate({ nome: RegExp(nome, "i") }, options);
+        return res.status(200).json(rotas);
       }
-      if (email) {
-        const usuarios = await Usuario.paginate({ email: RegExp(email, "i") }, options);
-        return res.status(200).json(usuarios);
-      }
-      if (nome && email) {
-        const usuarios = await Usuario.paginate({ email: RegExp(email, "i") }, options);
-        return res.status(200).json(usuarios);
-      }
-      if (ativo == true) {
-        const usuarios = await Usuario.paginate({ ativo: true }, options);
-        return res.status(200).json(usuarios)
-      }
-      const usuarios = await Usuario.paginate({}, options);
-      return res.status(200).json(usuarios);
+
+      const rotas = await Rotas.paginate({}, options);
+      return res.status(200).json(rotas);
 
     } catch (error) {
 
       return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
     }
 
-
   }
 
-  static listarUsuarioPorId = async (req, res) => {
+
+
+  static listarRotaPorId = async (req, res) => {
     try {
       const { id } = req.params;
+      const rota = await rotas.findById(id);
 
-      const usuario = await Usuario.findById(id);
-
-      if (!usuario) {
-        return res.status(404).json({ error: true, code: 404, message: "Usuário não encontrado" });
+      if (!rota) {
+        return res.status(404).json({ error: true, code: 404, message: "Id da rota não localizado." });
       }
 
-      return res.status(200).json(usuario);
-    } catch (error) {
-      return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
+      return res.status(200).json(rota);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: true, code: 500, message: "Erro interno do servidor" });
     }
   }
 
-  static atualizarUsuario = async (req, res) => {
+  static cadastrarRota = async (req, res) => {
     try {
-      const { id } = req.params;
-      const { nome, email, senha } = req.body;
+      const rota = new rotas(req.body);
+      await rota.save();
 
-      const usuarioAtualizado = await Usuario.findByIdAndUpdate(
-        id,
-        { nome, email, senha },
-        { new: true }
-      );
-
-      if (!usuarioAtualizado) {
-        return res.status(404).json({ error: true, code: 404, message: "Usuário não encontrado" });
-      }
-
-      return res.status(200).json(usuarioAtualizado);
-    } catch (error) {
-      return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
+      return res.status(201).send(rota.toJSON());
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: true, code: 500, message: "Erro interno do servidor" });
     }
   }
 
-  static deletarUsuario = async (req, res) => {
-    try {
-      const { id } = req.params;
-
-      const usuarioRemovido = await Usuario.findByIdAndRemove(id);
-
-      if (!usuarioRemovido) {
-        return res.status(404).json({ error: true, code: 404, message: "Usuário não encontrado" });
-      }
-
-      return res.status(204).json({ message: "Usuário removido com sucesso" });
-
-    } catch (error) {
-      return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
-    }
-  }
-
-
-  static atualizarUsuarioPatch = async (req, res) => {
+  static atualizarRota = async (req, res) => {
     try {
       const { id } = req.params;
       const updateData = req.body;
 
-      const usuarioAtualizado = await Usuario.findByIdAndUpdate(
-        id,
-        updateData,
-        { new: true }
-      );
+      const updatedRota = await rotas.findByIdAndUpdate(id, updateData);
 
-      if (!usuarioAtualizado) {
-        return res.status(404).json({ error: true, code: 404, message: "Usuário não encontrado" });
+      if (!updatedRota) {
+        return res.status(404).json({ error: true, code: 404, message: "Id da rota não localizado." });
       }
 
-      return res.status(200).json(usuarioAtualizado);
-    } catch (error) {
-      return res.status(500).json({ error: true, code: 500, message: "Erro interno no servidor" });
+      return res.status(200).json({ code: 200, message: "Operação bem sucedida" });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: true, code: 500, message: "Erro interno do servidor" });
     }
   }
 
+  static excluirRota = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedRota = await rotas.findByIdAndDelete(id);
 
+      if (!deletedRota) {
+        return res.status(404).json({ error: true, code: 404, message: "Id da rota não localizado." });
+      }
+
+      return res.status(200).send({ message: 'Rota removida com sucesso' });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: true, code: 500, message: "Erro interno do servidor" });
+    }
+  }
 }
 
+export default RotaController;
