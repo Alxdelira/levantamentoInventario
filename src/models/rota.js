c51c462d781c6901917a0470b16012df7c780261
Alexandre <alx.delira@gmail.com>
2023-05-30 09:41:43 -0400

    Implementando models de rota

---
+++ src/models/rota.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const rotaSchema = new mongoose.Schema(
+    {
+        rota: { type: String, required: true, trim: true, unique: true },
+        ativo: { type: Boolean, default: true },
+        verbo_get: { type: Boolean },
+        verbo_put: { type: Boolean },
+        verbo_patch: { type: Boolean },
+        verbo_delete: { type: Boolean },
+        verbo_post: { type: Boolean }
+    },
+    {
+        versionKey: false
+    }
+);
+
+rotaSchema.plugin(mongoosePaginate);
+
+const rotas = mongoose.model('rotas', rotaSchema);
+
+export default rotas;
