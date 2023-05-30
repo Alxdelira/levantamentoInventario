996df3232d0da73c95e6c528efe91de4ab0eaf53
Alexandre <alx.delira@gmail.com>
2023-05-30 09:34:54 -0400

    Alterando Seed para implementação e popular banco de dados

--- src/seeds/seeds.js
+++ src/seeds/seeds.js
@@ -1,8 +1,9 @@
 import faker from 'faker-br';
 import Setor from '../models/setor.js';
 import Item from '../models/item.js';
-import inventario from '../models/inventario.js';
+import Inventario from '../models/inventario.js';
 import Usuarios from '../models/usuario.js';
+import rotas from '../models/rota.js';
 import db from '../config/dbConect.js';
 import bcrypt from "bcrypt"
 
@@ -36,7 +37,6 @@ function getBlocoNome(i) {
 const setor = []
 
 const setorPadrao = {
-
     nome: "setor Padrao",
     bloco: "Bloco Padrao",
     ativo: true
@@ -67,22 +67,15 @@ console.log(setor.length + ' Setores inseridos!');
 await Item.deleteMany();
 const itens = [];
 
-function getItemID(qtd) {
-    for (let i = 0; i < qtd; i++) {
-        return itens[i]._id
-    }
-}
-
-console.log(getItemID(10))
 
 const itemPadrao = {
-    setor: setor[0]._id,
+    etiqueta: 77097,
     naoEtiquetado: false,
     encontrado: true,
-    etiqueta: 77097,
     nome: "cadeira",
-    descricao: "Uma cadeira muito legal",
+    setor: setor[0]._id,
     estado: "Em condições de uso",
+    descricao: "Uma cadeira muito legal",
     responsavel: "Sra. Maria Reis",
     imagem_url: "http://lorempixel.com/640/480",
     ativo: true,
@@ -93,17 +86,17 @@ itens.push(itemPadrao);
 function getSetorId(qtd) {
     for (let i = 0; i < qtd; i++) {
         return setor[i]._id
-        
     }
 }
-
 
 function seedItens(qtdItens) {
     for (let i = 0; i < qtdItens; i++) {
         const seedItem = {
             etiqueta: faker.lorem.words(),
+            naoEtiquetado: false,
+            encontrado: true,
             nome: faker.name.firstName(),
-            ativo: true,
+            setor: getSetorId(setor.length),
             estado: faker.random.arrayElement([
                 "Em condições de uso",
                 "Inservível",
@@ -112,7 +105,7 @@ function seedItens(qtdItens) {
             descricao: faker.lorem.sentence(),
             responsavel: faker.name.findName(),
             image: faker.image.imageUrl(),
-            setor: getSetorId(setor.length),
+            ativo: true,
         }
 
         itens.push(seedItem);
@@ -121,38 +114,97 @@ function seedItens(qtdItens) {
 
 seedItens(50);
 await Item.collection.insertMany(itens, { ordered: false });
+
 console.log(itens.length + " Itens inseridos!")
 
 // ------------------------------------------------------------
+// Inventarios Seeds
 
-// inventários
+// Deletar todos os inventários existentes
+await Inventario.deleteMany();
 
-await inventario.deleteMany();
+// Criação de itens
+const item1 = await Item.findOne();
+const item2 = await Item.findOne().skip(1);
 
 const inventarios = [];
 
+// Criação do inventário padrão
 const inventarioPadrao = {
-    setor: setor[0]._id,
-    itens: itemPadrao,
-    criadoEm: new Date()
-}
+  setor: setor[0]._id,
+  itens: [item1._id, item2._id],
+  criadoEm: new Date()
+};
 
 inventarios.push(inventarioPadrao);
 
-function seedInventario(qtdinventario,qtditens) {
-    for (let i = 0; i < qtdinventario; i++) {
-        inventarios.push({
-            setor: faker.random.arrayElement(setor),
-            bloco: faker.random.arrayElement(bloco_array),
-            itens: getItemID(qtditens)
-        })
-    }
+// Função para criar inventários aleatórios
+function seedInventario(qtdInventario) {
+  for (let i = 0; i < qtdInventario; i++) {
+    const inventario = {
+      setor: setor[i % setor.length]._id,
+      bloco: faker.random.arrayElement(bloco_array),
+      itens: [item1._id, item2._id]
+    };
+    inventarios.push(inventario);
+  }
 }
-seedInventario(5,10)
-await inventario.insertMany(inventarios);
+
+seedInventario(10);
+
+// Inserir os inventários no banco de dados
+await Inventario.insertMany(inventarios);
 console.log(inventarios.length + ' Inventários inseridos!');
 
 // ------------------------------------------------------------
+//eliminando as rotas existentes
+await rotas.deleteMany();
+
+// função para gerar array de objetos com dados fake para rotas
+const dadosRotas = [];
+
+// função para retornar o nome de uma rota pela posição do array
+let rotas_array = [
+    'setores', 'itens',
+    'usuarios', 'iventarios'
+]
+function getRotaName(i) {
+    return rotas_array[i].toString();
+}
+
+function seedRotas(qtdrotas) {
+    for (let i = 0; i < qtdrotas; i++) {
+        const rota = {
+            rota: getRotaName(i),
+            ativo: true,
+            verbos: {
+                get: true,
+                put: true,
+                patch: true,
+                delete: true,
+                post: true,
+            }
+        }
+        dadosRotas.push(rota);
+    }
+    return dadosRotas;
+}
+
+seedRotas(rotas_array.length);
+await rotas.collection.insertMany(dadosRotas);
+console.log(dadosRotas.length + ' Rotas inseridas!');
+
+// função para passar as rotas para os usuários
+const rotas_usuarios = [];
+function seedRotasUsuarios(qtd) {
+    // limpar array rotas_grupos
+    rotas_usuarios.length = 0;
+    for (let i = 0; i < (qtd); i++) {
+        rotas_usuarios.push(dadosRotas[i]);
+    }
+    return rotas_usuarios;
+};
+//_________________________________________________________
 
 // Usuario
 
@@ -160,54 +212,60 @@ await Usuarios.deleteMany();
 
 const usuarios = [];
 
-
 const usuarioPadrao = {
-    nome: "Mateus de Moraies",
-    email: "mateus@gmail.dev",
-    senha: "123",
-    ativo: true
-}
+    nome: 'Mateus de Moraies',
+    email: 'mateus@gmail.dev',
+    senha: '123',
+    ativo: true,
+    rota: [{
+        rota: "padrão",
+        ativo: true,
+        verbos: {
+            get: true,
+            put: true,
+            patch: true,
+            delete: true,
+            post: true,
+        }
+    }], // Array vazio para armazenar as rotas
+};
 
 usuarios.push(usuarioPadrao);
 
-
-
-
 function seedUsuario(qtdusuarios) {
     for (let i = 1; i <= qtdusuarios; i++) {
-        let nome = faker.name.firstName();
-        let nome_meio = faker.name.lastName();
-        let sobrenome = faker.name.lastName();
-        let email = nome + '.' + sobrenome + '@' + "gmail.com";
+        const nome = faker.name.firstName();
+        const nome_meio = faker.name.lastName();
+        const sobrenome = faker.name.lastName();
+        const email = `${nome}.${sobrenome}@gmail.com`;
 
-        const seedUsuarios =
-        {
-            nome: nome + ' ' + nome_meio + ' ' + sobrenome,
+        const seedUsuarios = {
+            nome: `${nome} ${nome_meio} ${sobrenome}`,
             email: email.toLowerCase(),
             senha: senhaHash(),
             ativo: true,
-            // link_foto: faker.image.avatar(),
-            // rotas: rotas,
-            // grupos: removerChaves(grupos)
-
+            rota: dadosRotas.map((rota) => {
+                return {
+                    rota: rota.rota,
+                    verbos: rota.verbos
+                };
+            }),
         };
+
         usuarios.push(seedUsuarios);
-        // console.log('Usuários ' + i + ' inseridos!');
     }
     return usuarios;
 }
 
-seedUsuario(5);
+seedUsuario(20);
 await Usuarios.collection.insertMany(usuarios);
-console.log(usuarios.length + ' Usuarios inseridos!');
+console.log(usuarios.length + ' Usuários inseridos!');
 
-// função para encrytar senha usando bcryptjs
-
+// Função para criptografar senha usando bcryptjs
 function senhaHash() {
     return bcrypt.hashSync('123', 8);
 }
 
-
-//Deligando a conexão com o banco de dados com mensagem de sucesso ou de erro
+// Desconectando do banco de dados com mensagem de sucesso ou erro
 db.close();
 console.log('Conexão com o banco encerrada!');
